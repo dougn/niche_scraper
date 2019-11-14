6d7c689bde916772b196f17ec5e3b0e66a11dbd5
Doug Napoleone <doug.napoleone+github@gmail.com>
2019-11-13 20:30:07 -0500

    Use selenium Safari
    Niche.com is now blocking scrapers, so use selenium to launch a browser in the background to do the actual page loading.
    
    More work and documentation required to show how to use this functionality, and currently hard set to Safari.

--- niche_scraper/util.py
+++ niche_scraper/util.py
@@ -4,6 +4,8 @@ import base64
 import os.path
 import shutil
 import requests
+import json
+import selenium
 from bs4 import BeautifulSoup
 from geopy import distance
 from .config import CONFIG
@@ -20,6 +22,27 @@ def b64string(string):
 
 def flush_cache():
     shutil.rmtree(CONFIG.user_cache_dir, ignore_errors=True)
+
+class DriverSingleton:
+    def __init__(self, 
+                 login_cookie_name=CONFIG.web.LoginCookieName, 
+                 login_cookie_value=CONFIG.web.LoginCookieValue):
+        self.d = None
+        self.login_cookie_name = login_cookie_name
+        self.login_cookie_value = login_cookie_value
+
+    @property
+    def driver(self):
+        if not self.d:
+            self.d = selenium.webdriver.Safari()
+            self.d.get('https://www.niche.com/account/')
+            self.d.add_cookie(
+                {'name' : self.login_cookie_name, 
+                 'value' : self.login_cookie_value, 
+                 'domain' : '.www.niche.com'})
+        return self.d
+
+Driver = DriverSingleton()
 
 def get_page(url, 
              cache=CONFIG.web.Cache, 
@@ -47,6 +70,28 @@ def get_page(url,
             data_conv = data.decode('utf-8')
             open(cachefilename, 'w').write(data_conv)
     return BeautifulSoup(data, 'html.parser')
+
+def get_page(url, 
+             cache=CONFIG.web.Cache, 
+             login_cookie_name=CONFIG.web.LoginCookieName, 
+             login_cookie_value=CONFIG.web.LoginCookieValue):
+    cachefilename = CONFIG.user_cache_dir.joinpath(b64string(url))
+    data = None
+    if cache and os.path.isfile(cachefilename):
+        data = open(cachefilename, 'r').read()
+    else:
+        Driver.driver.get(url)
+        data = Driver.driver.page_source
+        #har = json.loads(Driver.driver.get_log('har')[0]['message'])
+        #status = har['log']['entries'][0]['response']['status']
+        #if status in ['301', '302']:
+        #    return None
+        if cache:
+            os.makedirs(CONFIG.user_cache_dir, exist_ok=True)
+            data_conv = data.decode('utf-8')
+            open(cachefilename, 'w').write(data_conv)
+    return BeautifulSoup(data, 'html.parser')
+
 
 def _rescale_miles(base):
     l = math.log(base+1.0)
